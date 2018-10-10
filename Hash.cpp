890061dba3cf354ecfee7d9d74ed373090799be4
Lucas Barcellos Oliveira <lucas.barcellos.oliveira@poli.ufrj.br>
2018-10-10 20:52:09 -0300

    Implement hash's select

--- Hash.cpp
+++ Hash.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <cstring>
+#include <functional>
 
 Hash::Hash(const char* output) {
   this->blockp = new Block(output, 'w');
@@ -18,7 +19,26 @@ void ins(const char* record){
 }
 
 const Record * sel(const char* cpf){
-    
+  std::hash<std::string> hashFunction;
+  std::string cpfString(cpf);
+  this->pos = this->blockg->read(hashFunction(cpfString));
+  const Record* record;
+  for(int i = 0; i < this->blockg->count(); i++) {
+    record = this->blockg->get(i);
+    bool found = 1;
+    for (int j = 0; j < sizeof(record->cpf); j++) {
+      if (record->cpf[j] != cpf[j]) {
+        found = 0;
+        break;
+      }
+    }
+    if (found) {
+      std::cout << "Found record " << *record << std::endl;
+      return record;
+    }
+  }
+  std::cout << "No record with CPF = " << cpf << std::endl;
+  return nullptr;
 }
 
 void del(const char* cpf){
