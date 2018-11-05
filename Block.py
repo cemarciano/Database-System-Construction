dd253a36f7a6700de2afa8d19c0db90563c5bc66
João Victor Davim <joaovictor_rj@poli.ufrj.br>
2018-11-05 19:08:50 -0200

    Add read and write methods in Block class

--- Block.py
+++ Block.py
@@ -5,21 +5,36 @@ class Block:
     def __init__(self, disk_name):
         """ Open disk file in write/read mode and set number of operations to 0 """
         self.disk = open(disk_name, "w+")  # Disk file
-        self.n_r = 0  # Number of records
         self.max_size = 8  # Size of block
         self.n_op = 0  # Number of operations
         self.records = []  # List of records
 
     def __del__(self):
         self.persist(self.disk.tell())
+        self.disk.close()
 
     def persist(self, pos):
-        self.n_r = 0
         self.n_op += 1
         self.disk.seek(pos)
         for record in self.records:
             self.disk.write(record)
         self.disk.flush()
         self.records = []
-    # def write(self):
-    # def read(self):
+
+    def write(self, pos, record):
+        if (len(self.records) >= self.max_size):
+            self.persist(pos)
+        self.records += [record]
+
+    def read(self, pos):
+        self.n_op += 1
+        self.records = []
+        self.disk.seek(pos)
+        while (len(self.records) < self.max_size):
+            self.records += [self.disk.read(138)]
+
+# r = Record("11111111111;54.037.661-5;estermoro@gmail.com;06/01/1952;Feminino;Yuri Matheus Antonia;5942.00")
+# b = Block("out.txt")
+# b.write(0, str(r))
+# b.persist(0)
+# b.read(0)
