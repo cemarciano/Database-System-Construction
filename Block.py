70c5cc382b15030c3cc3352871164a7eaf2e6bd6
João Victor Davim <joaovictor_rj@poli.ufrj.br>
2018-11-05 22:21:55 -0200

    Add comments

--- Block.py
+++ Block.py
@@ -1,32 +1,46 @@
-from Record import *
+# from Record import *
 
 
 class Block:
     def __init__(self, disk_name):
-        """ Open disk file in write/read mode and set number of operations to 0 """
-        self.disk = open(disk_name, "w+")  # Disk file
-        self.max_size = 8  # Size of block
-        self.n_op = 0  # Number of operations
-        self.records = []  # List of records
+        """ Constructor """
+        # open disk file
+        self.disk = open(disk_name, "w+")
+        # set size of block
+        self.max_size = 8
+        # set number of operations (read/write)
+        self.n_op = 0
+        # set list of records
+        self.records = []
 
     def __del__(self):
+        """ Destructor """
+        # persist last records to disk
         self.persist(self.disk.tell())
+        # close disk file
         self.disk.close()
 
     def persist(self, pos):
+        """ Persists current block's records to the disk file """
+        # add 1 to number of operations
         self.n_op += 1
+        # position the pointer at the writing location
         self.disk.seek(pos)
-        for record in self.records:
-            self.disk.write(record)
+        # persist each block record to disk file
+        for rec in self.records:
+            self.disk.write(rec)
         self.disk.flush()
+        # reset list of records
         self.records = []
 
-    def write(self, pos, record):
+    def write(self, pos, rec):
+        """ Write record rec in position pos of the disk file """
         if (len(self.records) >= self.max_size):
             self.persist(pos)
-        self.records += [record]
+        self.records += [rec]
 
     def read(self, pos):
+        """ Add records of pos position in disk file to this """
         self.n_op += 1
         self.records = []
         self.disk.seek(pos)
--- Record.py
+++ Record.py
@@ -1,14 +1,27 @@
 class Record:
     def __init__(self, string):
         """ Split argument (record) by ; character and set member variables """
+        # set splited record
         field = string.split(';')
+        # set cpf
         self.cpf = field[0]
+        # set rg
         self.rg = field[1]
-        self.email = field[2] + '\0'*(40 - len(field[2]))
+        # set email
+        self.email = field[2] + '\0' * \
+            (40 - len(field[2])) if len(field[2]) < 40 else field[2][:40]
+        # set data de nascimento
         self.nasc = field[3]
-        self.sexo = field[4] + '\0'*(9 - len(field[4]))
-        self.nome = field[5] + '\0'*(40 - len(field[5]))
-        self.salario = field[6] + '\0'*(10 - len(field[6]))
+        # set sexo
+        self.sexo = field[4] + '\0' * \
+            (9 - len(field[4])) if len(field[4]) < 9 else field[4][:9]
+        # set nome
+        self.nome = field[5] + '\0'*(40 - len(field[5])
+                                     ) if len(field[5]) < 40 else field[5][:40]
+        # set salario
+        self.salario = field[6] + '\0' * \
+            (10 - len(field[6])) if len(field[6]) < 10 else field[6][:10]
 
     def __str__(self):
+        """ Return all member variables concatenated """
         return self.cpf+self.rg+self.email+self.nasc+self.sexo+self.nome+self.salario
