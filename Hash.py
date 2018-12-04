a97132b4daa106403faa0167bee3e0e431255a9c
Lucas Barcellos Oliveira <lucas.barcellos.oliveira@poli.ufrj.br>
2018-12-04 03:10:35 -0200

    Fix bug in B Tree indexing; Add B+ Tree indexing
    ("pip install bplustree" required)

--- Hash.py
+++ Hash.py
@@ -1,30 +1,46 @@
 from Record import *
 from Block import *
 from BTree import *
+from bplustree import BPlusTree, StrSerializer #pip install btreeplus
+#see https://github.com/NicolasLM/bplustree
+from os import getcwd
+from sys import byteorder
 
 class Hash:
-    def __init__(self, disk_name="Hash.cbd", indexBy=[], indexBTree=True):
+    def __init__(self, disk_name="Hash.cbd", indexBy=[], indexBTree=True, indexBPlusTree=False):
         maxDegreeBTree = 5
         self.r_block = Block(disk_name)
         self.w_block = Block(disk_name)
         self.indexes={}
         self.indexBTree = indexBTree
+        self.indexBPlusTree = indexBPlusTree
         for i in indexBy:
-            if indexBTree:
-                self.indexes.update({i:BTree(maxDegreeBTree)})
+            if indexBPlusTree:
+                self.indexes.update({i:BPlusTree(getcwd()+"\\"+i+".index",serializer=StrSerializer(),key_size=128)})
             else:
-                self.indexes.update({i:{}})
+                if indexBTree:
+                    self.indexes.update({i:BTree(maxDegreeBTree)})
+                else:
+                    self.indexes.update({i:{}})
+
+    def __del__(self):
+        if self.indexBPlusTree:
+            for i in indexBy:
+                self.indexes[i].close()
 
     def insert(self, string):
         rec = Record(string)
         self.w_block.write(abs(hash(rec.cpf)//10**6)*self.w_block.max_size*self.w_block.record_size, rec)
         self.w_block.persist(abs(hash(rec.cpf)//10**6)*self.w_block.max_size*self.w_block.record_size)
         for i in self.indexes:
-            if self.indexBTree:
-                self.indexes[i].insert(getattr(rec,i))
-                self.indexes[i].search(getattr(rec,i))[0].pos=abs(hash(rec.cpf)//10**6)*self.w_block.max_size*self.w_block.record_size
+            if self.indexBPlusTree:
+                self.indexes[i].insert(getattr(rec,i),abs(hash(rec.cpf)//10**6)*self.w_block.max_size*self.w_block.record_size.to_bytes(4,byteorder),True) #True for replacing node, if already existant
             else:
-                self.indexes[i].update({getattr(rec,i):abs(hash(rec.cpf)//10**6)*self.w_block.max_size*self.w_block.record_size})
+                if self.indexBTree:
+                    self.indexes[i].insert(getattr(rec,i))
+                    self.indexes[i].search(getattr(rec,i))[0].pos=abs(hash(rec.cpf)//10**6)*self.w_block.max_size*self.w_block.record_size
+                else:
+                    self.indexes[i].update({getattr(rec,i):abs(hash(rec.cpf)//10**6)*self.w_block.max_size*self.w_block.record_size})
 
     def join(self,other_hash,field,other_field=""):
         if not other_field:
@@ -41,10 +57,13 @@ class Hash:
                         print(i+"\n"+str(other_hash.r_block.records[0])+"\n")
                 else:
                     if other_field in other_hash.indexes: #if field is indexed
-                        if other_hash.indexBTree:
-                            other_hash.r_block.read(other_hash.indexes[other_field].search(getattr(Record(i),field))[0].pos)
+                        if other_hash.indexBPlusTree:
+                            other_hash.r_block.read(int.from_bytes(other_hash.indexes[other_field].get(getattr(Record(i),field),byteorder)))
                         else:
-                            other_hash.r_block.read(other_hash.indexes[other_field][getattr(Record(i),field)])
+                            if other_hash.indexBTree:
+                                other_hash.r_block.read(other_hash.indexes[other_field].search(getattr(Record(i),field))[0].pos)
+                            else:
+                                other_hash.r_block.read(other_hash.indexes[other_field][getattr(Record(i),field)])
                         for j in other_hash.r_block.records:
                             if not j:
                                 break
@@ -64,8 +83,10 @@ class Hash:
             pos+=self.r_block.max_size*self.r_block.record_size
             self.r_block.read(pos)
 
-a=Hash("teste1.cbd")
+a=Hash("teste1.cbd",indexBy=["nome"])
 a.insert("11111111111;54.037.661-5;estermoro@gmail.com;06/01/1952;Feminino;Yuri Matheus Antonia;5942.00")
-b=Hash("teste2.cbd")
+b=Hash("teste2.cbd",indexBy=["nome"])
 b.insert("11111111111;54.037.661-5;estermoro@gmail.com;06/01/1952;Feminino;Yuri Matheus Antonia;5942.00")
 a.join(b,"cpf")
+
+del a, b
--- Heap.py
+++ Heap.py
@@ -1,29 +1,45 @@
 from Record import *
 from Block import *
 from BTree import *
+from bplustree import BPlusTree, StrSerializer #pip install btreeplus
+#see https://github.com/NicolasLM/bplustree
+from os import getcwd
+from sys import byteorder
 
 
 class Heap:
-    def __init__(self, disk_name="Heap.cbd", indexBy=[], indexBTree=True):
+    def __init__(self, disk_name="Heap.cbd", indexBy=[], indexBTree=True, indexBPlusTree=False):
         maxDegreeBTree = 5
         self.r_block = Block(disk_name)
         self.w_block = Block(disk_name)
         self.indexes = {}
         self.indexBTree = indexBTree
+        self.indexBPlusTree = indexBPlusTree
         for i in indexBy:
-            if indexBTree:
-                self.indexes.update({i:BTree(maxDegreeBTree)})
+            if indexBPlusTree:
+                self.indexes.update({i:BPlusTree(getcwd()+"\\"+i+".index",serializer=StrSerializer(),key_size=128)})
             else:
-                self.indexes.update({i:{}})
+                if indexBTree:
+                    self.indexes.update({i:BTree(maxDegreeBTree)})
+                else:
+                    self.indexes.update({i:{}})
+
+    def __del__(self):
+        if self.indexBPlusTree:
+            for i in indexBy:
+                self.indexes[i].close()
 
     def insert(self, string):
         rec = Record(string)
         for i in self.indexes:
-            if self.indexBTree:
-                self.indexes[i].insert(getattr(rec,i))
-                self.indexes[i].search(getattr(rec,i))[0].pos=self.w_block.disk.tell()
+            if self.indexBPlusTree:
+                self.indexes[i].insert(getattr(rec,i),self.w_block.disk.tell().to_bytes(4,byteorder),True) #True for replacing node, if already existant
             else:
-                self.indexes[i].update({getattr(rec,i):self.w_block.disk.tell()})
+                if self.indexBTree:
+                    self.indexes[i].insert(getattr(rec,i))
+                    self.indexes[i].search(getattr(rec,i))[0].pos=self.w_block.disk.tell()
+                else:
+                    self.indexes[i].update({getattr(rec,i):self.w_block.disk.tell()})
         self.w_block.write(self.w_block.disk.tell(), rec)
 
     def join(self, other_heap, field, other_field=""):
@@ -36,10 +52,13 @@ class Heap:
                 if not i:
                     break
                 if other_field in other_heap.indexes:  # if field is indexed
-                    if other_heap.indexBTree:
-                        other_heap.r_block.read(other_heap.indexes[other_field].search(getattr(Record(i),field))[0].pos)
+                    if other_heap.indexBPlusTree:
+                        other_heap.r_block.read(int.from_bytes(other_heap.indexes[other_field].get(getattr(Record(i),field),byteorder)))
                     else:
-                        other_heap.r_block.read(other_heap.indexes[other_field][getattr(Record(i), field)]*other_heap.r_block.record_size)
+                        if other_heap.indexBTree:
+                            other_heap.r_block.read(other_heap.indexes[other_field].search(getattr(Record(i),field))[0].pos)
+                        else:
+                            other_heap.r_block.read(other_heap.indexes[other_field][getattr(Record(i), field)]*other_heap.r_block.record_size)
                     for j in other_heap.r_block.records:
                         if not j:
                             break
@@ -60,10 +79,12 @@ class Heap:
             self.r_block.read(pos)
 
 
-a = Heap("teste1.cbd")
+a=Heap("teste1.cbd",indexBy=["nome"])
 a.insert("11111111111;54.037.661-5;estermoro@gmail.com;06/01/1952;Feminino;Yuri Matheus Antonia;5942.00")
 a.insert("33333333333;54.037.661-5;estermoro@gmail.com;06/01/1952;Feminino;Yuri Matheus Antonia;5942.00")
-b=Heap("teste2.cbd")
+b=Heap("teste2.cbd",indexBy=["nome"])
 b.insert("22222222222;54.037.661-5;estermoro@gmail.com;06/01/1952;Feminino;Yuri Matheus Antonia;5942.00")
 
 a.join(b, "nome")
+
+del a, b
